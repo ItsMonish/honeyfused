64d9f9e48e1543501bf96656fdf93ea358fe62e3
Monish Kumar <73230556+ItsMonish@users.noreply.github.com>
2025-08-18 10:54:48 +0530

    added argument parsing and made debug logging optional

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 from filesystem import HoneyFileSystem
 import mfusepy as fuse
@@ -6,8 +7,26 @@ MOUNT_POINT = "/tmp/testing"
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
-    fuse.FUSE(HoneyFileSystem(), MOUNT_POINT, foreground=True)
+    args = parseArguments()
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    try:
+        fuse.FUSE(HoneyFileSystem(), MOUNT_POINT, foreground=True)
+    except RuntimeError:
+        print("[!!]: Mount point busy or doesn't exist")
+
+
+def parseArguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="HoneyFused",
+        description="A tool to deploy a decoy filesystem for intrusion detection",
+    )
+    parser.add_argument(
+        "-d", "--debug", action="store_true", help="Enable verbose debug output"
+    )
+    parser.add_argument("-c", "--config", type=str, help="Path to configuration file")
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == "__main__":
